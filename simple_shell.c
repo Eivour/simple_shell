fcd63970c1190c6e94626d7122001de1907ac52b
Eivour <laragoke91@gmail.com>
2023-11-15 16:54:59 +0100

    Making progress?

---
+++ simple_shell.c
@@ -0,0 +1,14 @@
+#include "shell.h"
+
+int main(int argcount, char **argvalue)
+{
+    char *message = "(fj_shell#) ~ ";
+    char *lineprinter;
+    size_t count = 0;
+
+    printf("%s", message);
+    getline(&lineprinter, &count, stdin);
+
+    free(lineprinter);
+    return (0);
+}
