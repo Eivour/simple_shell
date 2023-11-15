8a86626d97aea94a7abb0cf0caa7fe9f6e672205
Eivour <laragoke91@gmail.com>
2023-11-15 08:18:37 -0800

    betty

--- vour_print.c
+++ vour_print.c
@@ -8,12 +8,13 @@
 void vour_print(const char *format, ...)
 {
 	va_list args;
+
 	va_start(args, format);
 
 	char buffer[MAX_LINE];
+
 	vsnprintf(buffer, MAX_LINE, format, args);
 
 	write(STDOUT_FILENO, buffer, strlen(buffer));
-
 	va_end(args);
 }
